6c0c141d3db8a9be3d76176c6b81a1e09814192a
johnsonC <johnsonm.cameren@gmail.com>
2023-01-05 17:15:49 -0500

    give width responive base widths

--- src/components/ProjectDetailBanner/index.css.ts
+++ src/components/ProjectDetailBanner/index.css.ts
@@ -2,7 +2,8 @@ import { style } from "@vanilla-extract/css";
 
 export const bannerImgWrapper = style({
   position: "relative",
-  width: "100%",
+  width: "auto",
+  maxWidth: "100%",
   height: 'auto',
   maxHeight: "700px",
   
